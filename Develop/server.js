7daed8dbf0e0654693077cdb65f0b317f02683e2
charliecan2 <carloscq920@gmail.com>
2021-01-31 22:56:35 -0600

    Fixed the paths for the default and /notes destinations and returned all of the notes data to /api/notes as a JSON object

--- Develop/server.js
+++ Develop/server.js
@@ -2,16 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const fs = require('fs');
+
 const PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('/api/notes', (req, res) => {
+    const database = fs.readFileSync('./db/db.json')
+    const dataParse = JSON.parse(database);
+    res.json(dataParse);
+})
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
 })
 
 app.listen(PORT, () => {
