d5992988e98de3209a5f3e8527c2b10187c48f70
ccantu20 <carloscq920@gmail.com>
2021-02-03 21:46:01 -0600

    Added a Procfile

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const fs = require('fs');
 
-const PORT = 3000;
+const PORT = PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
