7b3370da442c57d7a4f891d514c4f7e5f9511eb1
ccantu20 <carloscq920@gmail.com>
2021-02-04 09:46:45 -0600

    Added a public directory

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
